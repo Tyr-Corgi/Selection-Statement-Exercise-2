9df3c9d46369dee005fff306a69d2bb7adc3717c
Tyler Gaisford <tyler.gaisford@gmail.com>
2023-03-08 13:50:50 -0700

    Completed Exercise2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -3,8 +3,48 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {   //User inputs
+            Console.WriteLine("What is your name?");
+                var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {userName}, What is your favorite school subject?");
+                var schoolSubject = Console.ReadLine();
+
+            //Switch Expression
+            switch (schoolSubject.ToLower()) 
+            {
+                //Math
+                case "math":
+                case "mathmatics":
+                    Console.WriteLine("My brain is mostly math and electricity"); 
+                    break;
+
+                //History
+                case "history":
+                    Console.WriteLine("History doesn't repeat but it does ryhme"); 
+                    break;
+
+                //Science
+                case "science":
+                    Console.WriteLine("I was made by science and powered by Corgi's");
+                    break;
+
+                //Gym
+                case "gym":
+                    Console.WriteLine("Like every subject, wither you get something out of it depends on how much you put into it. ");
+                    break;
+
+                //English
+                case "english":
+                    Console.WriteLine("If you can read this, you are doing at least mediocre in your studies");
+                    break;
+                
+                //Default
+                default:
+                    Console.WriteLine("I have never heard of that subject... you are unique.");
+                    break;
+            }
+
         }
     }
 }
